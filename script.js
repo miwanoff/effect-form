261f6d63f1653ac63d7a6a8ce03bbb073293227f
miwanoff <marina.iva.noffa@gmail.com>
2022-12-25 13:11:33 +0200

    Another way

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// let labels = document.querySelectorAll("label");
+// for (let i = 0; i < labels.length; i++) {
+//   let time = 0;
+//   // console.log(labels[i].innerHTML);
+//   let myArray = labels[i].innerHTML.split("");
+//   // console.log(myArray);
+//   let newInnerHTML = "";
+//   for (let j = 0; j < myArray.length; j++) {
+//     let element = `<span style="transition-delay:${time}ms;">${myArray[j]}</span>`;
+//     // console.log(element);
+//     newInnerHTML += element;
+//     time += 50;
+//   }
+//   // console.log(newInnerHTML);
+//   labels[i].innerHTML = newInnerHTML;
+// }
+
 let labels = document.querySelectorAll("label");
-for (let i = 0; i < labels.length; i++) {
-  let time = 0;
-  console.log(labels[i].innerHTML);
-  let myArray = labels[i].innerHTML.split("");
-  console.log(myArray);
-  let newInnerHTML = "";
-  for (let j = 0; j < myArray.length; j++) {
-    let element = `<span style="transition-delay:${time}ms;">${myArray[j]}</span>`;
-    console.log(element);
-    newInnerHTML += element;
-    time += 50;
-  }
-  console.log(newInnerHTML);
-  labels[i].innerHTML = newInnerHTML;
+labels.forEach(f);
+
+function f(label) {
+  label.innerHTML = label.innerText.split("").map(mapf).join("");
 }
+
+function mapf(item, index) {
+  //return item + "!!!";
+  return `<span style="transition-delay:${index * 50}ms;">${item}</span>`;
+}
+
+
